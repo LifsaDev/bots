68204665ced113d9ce8c428dbccd1237eed7ee1c
Salifou Saouadogo <salifou.saouadogo@jesagroup.com>
2023-11-04 20:36:39 +0100

    update

---
+++ bot.py
@@ -0,0 +1,222 @@
+import os
+import logging
+import click
+import torch
+import qa_to_csv
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.llms import HuggingFacePipeline
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler  # for streaming response
+from langchain.callbacks.manager import CallbackManager
+
+
+import os
+import sys
+
+class suppress_stdout_stderr(object):
+    def __enter__(self):
+        self.outnull_file = open(os.devnull, 'w')
+        self.errnull_file = open(os.devnull, 'w')
+
+        self.old_stdout_fileno_undup  = sys.stdout.fileno()
+        self.old_stderr_fileno_undup    = sys.stderr.fileno()
+
+        self.old_stdout_fileno = os.dup ( sys.stdout.fileno() )
+        self.old_stderr_fileno = os.dup ( sys.stderr.fileno() )
+
+        self.old_stdout = sys.stdout
+        self.old_stderr = sys.stderr
+
+        os.dup2 ( self.outnull_file.fileno(), self.old_stdout_fileno_undup )
+        os.dup2 ( self.errnull_file.fileno(), self.old_stderr_fileno_undup )
+
+        sys.stdout = self.outnull_file        
+        sys.stderr = self.errnull_file
+        return self
+
+    def __exit__(self, *_):        
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
+
+        os.dup2 ( self.old_stdout_fileno, self.old_stdout_fileno_undup )
+        os.dup2 ( self.old_stderr_fileno, self.old_stderr_fileno_undup )
+
+        os.close ( self.old_stdout_fileno )
+        os.close ( self.old_stderr_fileno )
+
+        self.outnull_file.close()
+        self.errnull_file.close()
+
+
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+from prompt import get_prompt_template
+ 
+from langchain.vectorstores import Chroma
+from transformers import (
+    GenerationConfig,
+    pipeline,
+)
+
+from load_models import (
+    load_quantized_model_gguf_ggml,
+    load_quantized_model_qptq,
+    load_full_model,
+)
+
+from constants import (
+    EMBEDDING_MODEL_NAME,
+    PERSIST_DIRECTORY,
+    MODEL_ID,
+    MODEL_BASENAME,
+    MAX_NEW_TOKENS,
+    MODELS_PATH,
+    CHROMA_SETTINGS
+)
+
+
+def load_model(device_type, model_id, model_basename=None, LOGGING=logging):
+    if model_basename is not None:
+        if ".gguf" in model_basename.lower():
+            with suppress_stdout_stderr():
+                llm = load_quantized_model_gguf_ggml(model_id, model_basename, device_type, LOGGING)
+                return llm
+        elif ".ggml" in model_basename.lower():
+            model, tokenizer = load_quantized_model_gguf_ggml(model_id, model_basename, device_type, LOGGING)
+        else:
+            model, tokenizer = load_quantized_model_qptq(model_id, model_basename, device_type, LOGGING)
+    else:
+        model, tokenizer = load_full_model(model_id, model_basename, device_type, LOGGING)
+
+    generation_config = GenerationConfig.from_pretrained(model_id)
+    
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_length=MAX_NEW_TOKENS,
+        temperature=0.2,
+        # top_p=0.95,
+        repetition_penalty=1.15,
+        generation_config=generation_config,
+    )
+
+    local_llm = HuggingFacePipeline(pipeline=pipe)
+    return local_llm
+
+
+def retrieval_qa_pipline(device_type, use_history, promptTemplate_type="llama"):
+    embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": device_type})
+     
+    db = Chroma(
+        persist_directory=PERSIST_DIRECTORY,
+        embedding_function=embeddings,
+        client_settings=CHROMA_SETTINGS
+    )
+    retriever = db.as_retriever(search_kwargs={"k":3})
+    prompt, memory = get_prompt_template(promptTemplate_type=promptTemplate_type, history=use_history)
+    llm = load_model(device_type, model_id=MODEL_ID, model_basename=MODEL_BASENAME, LOGGING=logging)
+
+    if use_history:
+        qa = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",  
+            retriever=retriever,
+            return_source_documents=True,  # verbose=True,
+            callbacks=callback_manager,
+            chain_type_kwargs={"prompt": prompt, "memory": memory},
+        )
+    else:
+        qa = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff", 
+            retriever=retriever,
+            return_source_documents=True,  # verbose=True,
+            callbacks=callback_manager,
+            chain_type_kwargs={
+                "prompt": prompt,
+            },
+        )
+
+    return qa
+
+
+# choose device type to run model.
+@click.command()
+@click.option(
+    "--device_type",
+    default="cuda" if torch.cuda.is_available() else "cpu",
+    type=click.Choice(
+        [
+            "cpu",
+            "cuda",
+            "ipu",
+            "xpu",
+            "mkldnn",
+            "opengl",
+            "opencl",
+            "ideep",
+            "hip",
+            "ve",
+            "fpga",
+            "ort",
+            "xla",
+            "lazy",
+            "vulkan",
+            "mps",
+            "meta",
+            "hpu",
+            "mtia",
+        ],
+    ),
+    help="Device to run on. (Default is cuda)",
+)
+@click.option(
+    "--show_sources",
+    "-s",
+    is_flag=True,
+    help="Show sources along with answers (Default is False)",
+)
+@click.option(
+    "--use_history",
+    "-h",
+    is_flag=True,
+    help="Use history (Default is False)",
+)
+@click.option(
+    "--model_type",
+    default="llama",
+    type=click.Choice(
+        ["llama", "mistral", "non_llama"],
+    ),
+    help="model type, llama, mistral or non_llama",
+)
+@click.option(
+    "--save_qa",
+    is_flag=True,
+    help="save Q&A pairs to a CSV file",
+)
+
+def main(device_type, show_sources, use_history, model_type, save_qa):
+    if not os.path.exists(MODELS_PATH):
+        os.mkdir(MODELS_PATH)
+
+    qa = retrieval_qa_pipline(device_type, use_history, promptTemplate_type=model_type)
+    while True:
+        query = input("\nEnter a query: ")
+        if query == "exit":
+            break
+        with suppress_stdout_stderr():
+            res = qa(query)
+        answer, docs = res["result"], res["source_documents"]
+
+        print("\n\n> Question:")
+        print(query)
+        print("\n> Answer:")
+        print(answer)
+
+        
+
+if __name__ == "__main__":
+    main()
